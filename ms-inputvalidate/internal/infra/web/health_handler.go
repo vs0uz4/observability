9c7e1fa52d33600f28a6f8a44563388f44b550e1
vs0uz4 <vitor.rodrigues@gmail.com>
2024-12-31 13:49:10 -0300

    fix headers and add annotations

--- ms-inputvalidate/internal/infra/web/health_handler.go
+++ ms-inputvalidate/internal/infra/web/health_handler.go
@@ -15,6 +15,14 @@ func NewHealthHandler(u usecase.HealthCheckUseCase) *HealthHandler {
 	return &HealthHandler{u}
 }
 
+// GetHealth é o handler responsável por retornar informações sobre a saúde do serviço
+// @Summary Retorna informações sobre a saúde do serviço
+// @Description Retorna informações sobre a saúde do serviço, como uso de CPU, memória e tempo de atividade
+// @Tags Health
+// @Produce  json
+// @Success 200 {object} health.HealthStats
+// @Failure 500 {string} string "Internal Server Error"
+// @Router /health [get]
 func (h *HealthHandler) GetHealth(w http.ResponseWriter, r *http.Request) {
 	health, err := h.useCase.GetHealth()
 	if err != nil {
@@ -22,9 +30,11 @@ func (h *HealthHandler) GetHealth(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	err = json.NewEncoder(w).Encode(health)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	w.WriteHeader(http.StatusOK)
 }
