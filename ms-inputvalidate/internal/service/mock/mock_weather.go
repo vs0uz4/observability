79f95c069b431139e118615897e3989ba35a86fb
vs0uz4 <vitor.rodrigues@gmail.com>
2025-01-01 16:02:10 -0300

    update mocks and your tests in ms-inputvalidate

--- ms-inputvalidate/internal/service/mock/mock_weather.go
+++ ms-inputvalidate/internal/service/mock/mock_weather.go
@@ -1,11 +1,15 @@
 package mock
 
-import "github.com/vs0uz4/observability/ms-inputvalidate/internal/domain"
+import (
+	"context"
+
+	"github.com/vs0uz4/observability/ms-inputvalidate/internal/domain"
+)
 
 type MockWeatherService struct {
-	GetWeatherFunc func(string) (domain.WeatherResponse, error)
+	GetWeatherFunc func(ctx context.Context, cep string) (domain.WeatherResponse, error)
 }
 
-func (m *MockWeatherService) GetWeather(cep string) (domain.WeatherResponse, error) {
-	return m.GetWeatherFunc(cep)
+func (m *MockWeatherService) GetWeather(ctx context.Context, cep string) (domain.WeatherResponse, error) {
+	return m.GetWeatherFunc(ctx, cep)
 }
--- ms-inputvalidate/internal/service/mock/mock_weather_test.go
+++ ms-inputvalidate/internal/service/mock/mock_weather_test.go
@@ -1,6 +1,7 @@
 package mock
 
 import (
+	"context"
 	"testing"
 
 	"github.com/vs0uz4/observability/ms-inputvalidate/internal/domain"
@@ -8,7 +9,7 @@ import (
 
 func TestMockWeatherService(t *testing.T) {
 	mock := MockWeatherService{
-		GetWeatherFunc: func(cep string) (domain.WeatherResponse, error) {
+		GetWeatherFunc: func(ctx context.Context, cep string) (domain.WeatherResponse, error) {
 			switch cep {
 			case "12345678":
 				return domain.WeatherResponse{City: "ABC", Celsius: 24.2, Fahrenheit: 75.6, Kelvin: 297.33}, nil
@@ -22,24 +23,37 @@ func TestMockWeatherService(t *testing.T) {
 		},
 	}
 
-	response, err := mock.GetWeather("12345678")
-	if response.Celsius != 24.2 && response.City != "ABC" || err != nil {
-		t.Errorf("Expected Celsius: 24.2 and City: ABC, got: %v, err: %v", response.Celsius, err)
-	}
+	ctx := context.Background()
 
-	expectedMessage := "Expected error: %v, got: %v"
-	_, err = mock.GetWeather("123")
-	if err != domain.ErrInvalidZipcode {
-		t.Errorf(expectedMessage, domain.ErrInvalidZipcode, err)
-	}
+	t.Run("Success", func(t *testing.T) {
+		response, err := mock.GetWeather(ctx, "12345678")
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
 
-	_, err = mock.GetWeather("87654321")
-	if err != domain.ErrZipcodeNotFound {
-		t.Errorf(expectedMessage, domain.ErrZipcodeNotFound, err)
-	}
+		if response.Celsius != 24.2 || response.City != "ABC" {
+			t.Errorf("Expected Celsius: 24.2 and City: ABC, got: %v and %v", response.Celsius, response.City)
+		}
+	})
 
-	_, err = mock.GetWeather("23456789")
-	if err != domain.ErrInternalServer {
-		t.Errorf(expectedMessage, domain.ErrInternalServer, err)
-	}
+	t.Run("Invalid ZipCode Failure", func(t *testing.T) {
+		_, err := mock.GetWeather(ctx, "123")
+		if err != domain.ErrInvalidZipcode {
+			t.Errorf("Expected error: %v, got: %v", domain.ErrInvalidZipcode, err)
+		}
+	})
+
+	t.Run("ZipCode Not Found Failure", func(t *testing.T) {
+		_, err := mock.GetWeather(ctx, "87654321")
+		if err != domain.ErrZipcodeNotFound {
+			t.Errorf("Expected error: %v, got: %v", domain.ErrZipcodeNotFound, err)
+		}
+	})
+
+	t.Run("Unexpected Error Failure", func(t *testing.T) {
+		_, err := mock.GetWeather(ctx, "23456789")
+		if err != domain.ErrInternalServer {
+			t.Errorf("Expected error: %v, got: %v", domain.ErrInternalServer, err)
+		}
+	})
 }
