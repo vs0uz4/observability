1c4d1a3113d97a46a4b140c6569ad8298a1f3795
vs0uz4 <vitor.rodrigues@gmail.com>
2025-01-01 16:11:45 -0300

    update usecase and tests in ms-inputvalidate

--- ms-inputvalidate/internal/usecase/weather-location_by_cep.go
+++ ms-inputvalidate/internal/usecase/weather-location_by_cep.go
@@ -1,9 +1,13 @@
 package usecase
 
 import (
+	"context"
+
 	"github.com/vs0uz4/observability/ms-inputvalidate/internal/domain"
 	"github.com/vs0uz4/observability/ms-inputvalidate/internal/infra/utils"
 	"github.com/vs0uz4/observability/ms-inputvalidate/internal/service/contract"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 )
 
 type weatherLocationByCepUsecase struct {
@@ -16,13 +20,20 @@ func NewWeatherLocationByCepUsecase(weatherService contract.WeatherService) *wea
 	}
 }
 
-func (uc *weatherLocationByCepUsecase) GetWeatherLocationByCep(cep string) (domain.WeatherResponse, error) {
+func (uc *weatherLocationByCepUsecase) GetWeatherLocationByCep(ctx context.Context, cep string) (domain.WeatherResponse, error) {
+	tracer := otel.Tracer("ms-inputvalidate")
+	ctx, span := tracer.Start(ctx, "GetWeatherLocationByCep")
+	span.SetAttributes(attribute.String("cep", cep))
+	defer span.End()
+
 	if len(cep) != 8 || !utils.IsNumeric(cep) {
+		span.RecordError(domain.NewInvalidZipCodeDetailsError(cep))
 		return domain.WeatherResponse{}, domain.ErrInvalidZipcode
 	}
 
-	weather, err := uc.WeatherService.GetWeather(cep)
+	weather, err := uc.WeatherService.GetWeather(ctx, cep)
 	if err != nil {
+		span.RecordError(err)
 		return domain.WeatherResponse{}, err
 	}
 
