51e531ae247a91675194caf3a9e40ce928789aaf
vs0uz4 <vitor.rodrigues@gmail.com>
2025-01-01 15:20:17 -0300

    create opentelemetry pkg

--- pkg/tracing/tracer.go
+++ pkg/tracing/tracer.go
@@ -4,10 +4,11 @@ import (
 	"context"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/zipkin"
+	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
 func InitTracer(serviceName string, zipkinURL string) (*sdktrace.TracerProvider, error) {
@@ -19,10 +20,16 @@ func InitTracer(serviceName string, zipkinURL string) (*sdktrace.TracerProvider,
 	tp := sdktrace.NewTracerProvider(
 		sdktrace.WithBatcher(exporter),
 		sdktrace.WithResource(resource.NewWithAttributes(
-			attribute.String("service.name", serviceName),
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(serviceName),
 		)),
 	)
 	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	))
+
 	return tp, nil
 }
 
